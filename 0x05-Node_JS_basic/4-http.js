0f1c4ca15dbf803bf8e33780a69002531e83ad2b
JI-Maina <irungujmaina@gmail.com>
2023-08-22 14:34:14 +0000

    Create a small HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,6 @@
 const app = require('http');
 
 const port = 1245;
-const host = '127.0.0.1';
 
 const server = app.createServer((req, res) => {
   res.statusCode = (200);
@@ -9,7 +8,7 @@ const server = app.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, host, () => {
+server.listen(port, 'localhost', () => {
   console.log('...');
 });
 

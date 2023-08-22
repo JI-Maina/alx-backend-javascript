26165874c7417d5a66e8968f87fef9a22c86b604
JI-Maina <irungujmaina@gmail.com>
2023-08-22 12:28:51 +0000

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (fs.existsSync(path)) {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      const lines = data.split('\n');
+      const newLines = lines.filter((line) => line !== '');
+      const finalLines = newLines.splice(1);
+      const items = [];
+
+      for (const line of finalLines) {
+        const [firstname, lastname, age, field] = line.split(',');
+        const item = {
+          firstname, lastname, age, field,
+        };
+        items.push(item);
+      }
+
+      console.log(`Number of students: ${finalLines.length}`);
+
+      const fields = {};
+      for (const item of items) {
+        const key = item.field;
+
+        if (!fields[key]) {
+          fields[key] = [];
+        }
+
+        fields[key].push(item);
+      }
+
+      for (const [key, value] of Object.entries(fields)) {
+        const len = value.length;
+
+        const list = [];
+        for (const idx of value) {
+          list.push(idx.firstname);
+        }
+
+        console.log(`Number of students in ${key}: ${len}. List: ${list}`);
+      }
+    });
+  } else {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

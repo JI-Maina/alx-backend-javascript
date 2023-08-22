870dff0e25fb8958ca306cf097cdc68b8dd71c8a
JI-Maina <irungujmaina@gmail.com>
2023-08-22 16:10:22 +0000

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(path)) {
+      fs.readFile(path, 'utf-8', (err, data) => {
+        if (err) {
+          reject(err);
+        }
+
+        const lines = data.split('\n');
+        const newLines = lines.filter((line) => line !== '');
+        const finalLines = newLines.splice(1);
+        const items = [];
+
+        for (const line of finalLines) {
+          const [firstname, lastname, age, field] = line.split(',');
+          const item = {
+            firstname, lastname, age, field,
+          };
+          items.push(item);
+        }
+
+        console.log(`Number of students: ${finalLines.length}`);
+
+        const fields = {};
+        for (const item of items) {
+          const key = item.field;
+
+          if (!fields[key]) {
+            fields[key] = [];
+          }
+
+          fields[key].push(item);
+        }
+
+        for (const [key, value] of Object.entries(fields)) {
+          const len = value.length;
+
+          const names = () => {
+            let nameString = '';
+            for (const idx of value) {
+              nameString += `${idx.firstname}, `;
+            }
+            return nameString.trim().slice(0, -1);
+          };
+
+          resolve(console.log(`Number of students in ${key}: ${len}. List: ${names()}`));
+        }
+      });
+    } else {
+      throw new Error('Cannot load the database');
+    }
+  });
+}
+
+module.exports = countStudents;
